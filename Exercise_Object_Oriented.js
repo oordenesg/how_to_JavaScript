be6baca2f7982d068d2dc55c343e955ce28ec98f
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-05-01 11:42:16 -0400

    Update and rename Exercise_Object_Oriented.md to Exercise_Object_Oriented.js

--- Exercise_Object_Oriented.js
+++ Exercise_Object_Oriented.js
@@ -1,8 +1,7 @@
-## Team Stats
+// Team Stats
 
-We want to create and extract information about your favorite sports team. Basketball, soccer, tennis, or water polo, you pick it. It’s your job to create data using the JavaScript data structures at your disposal: arrays, objects, etc. Once created, you can manipulate these data structures as well as gain insights from them. For example, you might want to get the total number of games your team has played, or the average score of all of their games.
+// We want to create and extract information about your favorite sports team. Basketball, soccer, tennis, or water polo, you pick it. It’s your job to create data using the JavaScript data structures at your disposal: arrays, objects, etc. Once created, you can manipulate these data structures as well as gain insights from them. For example, you might want to get the total number of games your team has played, or the average score of all of their games.
 
-```javascript
 const team = {
   _players:[{firstName: 'Pablo',lastName: 'Sanchez',age:11},{firstName: 'Sergio',lastName:'Gonzalez',age:14},{firstName:'Juan',lastName:'Oviedo',age:13}],
   _games:[{opponent:'Broncos',teamPoints:42,opponentPoints:27},{opponent:'Toronto',teamPoints:38,opponentPoints:33},{opponent:'Florida',teamPoints:44,opponentPoints:42}],
@@ -39,4 +38,4 @@ console.log(team.games)
 
 team.addGames('LA','45','54')
 console.log(team.games)
-```
+
