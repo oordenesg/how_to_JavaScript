2341e74f39e53d783ee618f580bda634a85a16c2
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-05-02 12:17:38 -0400

    Update Exercise_Object_Oriented_1.js

--- Exercise_Object_Oriented_1.js
+++ Exercise_Object_Oriented_1.js
@@ -1,7 +1,18 @@
 // Meal Maker
 // As a frequent diner, you love trying out new restaurants and experimenting with different foods. However, having to figure out what you want to order can be a time-consuming ordeal if the menu is big, and you want an easier way to be able to figure out what you are going to eat.
-
 // In this project, you’ll use JavaScript to randomly create a three-course meal based on what is available on a menu. We’ll keep running it until we’re satisfied with the generated meal!
+//1. Create an empty object called menu .
+//2. Add _course property to our menu object. This should be an empty object.
+//3. Add three properties inside the _course object. These properties will be appetizers, mains and desserts
+//4. Create getter and setter methods for the appetizers, mains and desserts
+//5. Inside menu, create an empty getter method for the course property
+//6. Whitin this getter method, return an object that contains key/value pairs for _course
+//7. Inside menu, create a method called .addDishToCourse. This new method should take three parameters courseName,DishName,DishPrice.
+//8. The .addDhisToCourse method should create an object called dish which has *name and price properties. The method should then push this dish object into the appropriate array in your menu‘s _courses object based on what courseName was passed in.
+//9. Inside menu, create a method called .getRandomDishFromCourse(). This method should take one parameter called courseName.
+//10. Inside the method .getRandomDishFromCourse, retrieve the array of the given course’s dishes from the menu‘s _courses object and store in a variable called dishes. Additionally, create a random index using length of the dishes array. Remember to use Math.floor. Then, return the dish located at that index in dishes.
+//11. Create a function called .generateRandomMeal(). This funcion will generate a three-course meals and it does not take any parameters. This function should create three variables appetizer, main, dessert. For each of them we just have to call the .getRandomDishFromCourse() method. Then, create a new variable with the total price and return a sentenc with the variables and the total price.
+//12. Now, we just have create a menu by adding some appetizers, mains and desserts. Try your program.
 
 
 const menu = {
