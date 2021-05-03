8acaad5da5b960143dc955858258f11dcaebec55
Oscar Ordenes <76072249+oordenesg@users.noreply.github.com>
2021-05-03 18:08:46 -0400

    Update Code_Challenges_Intermediate_JS.js

--- Code_Challenges_Intermediate_JS.js
+++ Code_Challenges_Intermediate_JS.js
@@ -77,3 +77,12 @@ const sortYears = (array) => {
 const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 console.log(sortYears(years))
 
+//8. Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays.
+
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
+
